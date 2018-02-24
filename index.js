a30075772b9172c168189c9c5b7c4af1a3d431ac
chazzhou <chazzhou@tencent.com>
2018-02-24 16:43:09 +0800

    [!] 不应该应用投影矩阵，浏览器内部可能会自己处理，浏览器间表现方式不一

--- index.js
+++ index.js
@@ -14,14 +14,32 @@ let face = new CSS3D.Face({
   width: 100,
   height: 100
 });
+let face2 = new CSS3D.Face({
+  width: 100,
+  height: 100
+});
+face2.elem.style.background = 'red';
 
-camera.setPosition([100, 100, 200]);
-camera.setLookAt([0, 0, -200]);
-face.setPosition([0, 0, -200]);
-face.setPosition([0, 0, -200]);
+camera.setPosition([0, 100, 200]);
+camera.setLookAt([0, 0, 0]);
+face.setPosition([0, 0, 0]);
+face.setPosition([0, 0, 0]);
+
 // face.setScale([0.5, 1, 1]);
-// face.setRotation([Math.PI / 4, 0, 0]);
-
+face.setRotation([Math.PI / -2, 0, 0]);
+// face2.setRotation([Math.PI / -2, 0, 0]);
 scene.add(face);
+scene.add(face2);
+
+
+let cameraRadian = 0;
+function tick(){
+  cameraRadian += 0.006;
+
+  camera.setPosition([Math.cos(cameraRadian) * 300, 200, Math.sin(cameraRadian) * 300]);
+  CSS3D.render(scene, camera);
+  window.requestAnimationFrame(tick);
+}
+// tick();
 
 CSS3D.render(scene, camera);
